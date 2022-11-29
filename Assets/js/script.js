4242b6a1f69acbd760ffd404da795ddec627b5fc
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2022-11-29 15:42:23 -0400

    DOM & global vars

---
+++ Assets/js/script.js
@@ -0,0 +1,5 @@
+// defining DOM elements and global variables
+let outputEl = document.getElementById("output");
+let searchButton = document.getElementById("search-button");
+let historyContainer = document.getElementById("history-container");
+let fiveDayOutput = document.getElementById("five-day-output");
